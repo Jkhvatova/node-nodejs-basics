49a888f8c97824a6ebf7b8f60a75c32cb626bd30
Julia Khvatova <jkhvatova@gmail.com>
2024-01-30 19:37:37 +0300

    feat: solve fs/read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import * as fs from 'node:fs/promises';
+import path from "node:path";
+import { fileURLToPath } from 'url'
+
 const read = async () => {
-    // Write your code here 
+    const __dirname =  path.dirname(fileURLToPath(import.meta.url));
+    try {
+        const fileToRead = await fs.readFile(path.join(__dirname, 'files', 'fileToRead.txt'), 'utf-8');
+        console.log(fileToRead);
+    } catch(error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+
+        } else {
+            throw error;
+        }
+    }
 };
 
 await read();
