99f85b620ee48a14f6649a65959ea9243b689f3a
Julia Khvatova <jkhvatova@gmail.com>
2024-01-30 19:16:47 +0300

    feat: solve fs/delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import * as fs from 'node:fs/promises';
+import path from "node:path";
+import { fileURLToPath } from 'url'
+
 const remove = async () => {
-    // Write your code here 
+    const __dirname =  path.dirname(fileURLToPath(import.meta.url));
+    const fileToRemove = path.join(__dirname, 'files', 'fileToRemove.txt');
+
+    try {
+        await fs.unlink(fileToRemove);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+
+        } else {
+            throw error;
+        }
+    }
 };
 
 await remove();
