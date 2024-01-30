cf3d45d46eae1aab950e68bccbe21210e6bf476b
Julia Khvatova <jkhvatova@gmail.com>
2024-01-30 21:49:15 +0300

    feat: solve cli/args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const result = [];
+    args.forEach((arg, i) => {
+        if (arg.startsWith('--')) {
+            result.push(`${args[i].slice(2)} is ${args[i+1]}`);
+        }
+    })
+    console.log(result.join(', '));
 };
 
 parseArgs();
+
+
