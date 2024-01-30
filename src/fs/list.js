21b68c58e07663ad3231ce532a47ae682fd0e586
Julia Khvatova <jkhvatova@gmail.com>
2024-01-30 19:28:04 +0300

    feat: solve fs/list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import * as fs from 'node:fs/promises';
+import path from "node:path";
+import { fileURLToPath } from 'url'
+
 const list = async () => {
-    // Write your code here 
+    const __dirname =  path.dirname(fileURLToPath(import.meta.url));
+    try {
+        const files = await fs.readdir(path.join(__dirname, 'files'));
+        console.log(files);
+    } catch(error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+
+        } else {
+            throw error;
+        }
+    }
 };
 
 await list();
