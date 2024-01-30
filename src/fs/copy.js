59ae6601fd2e1b525b429f4a98b633a691ce1556
Julia Khvatova <jkhvatova@gmail.com>
2024-01-30 15:08:00 +0300

    feat: solve copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import * as fs from 'node:fs/promises';
+import path from "node:path";
+import { fileURLToPath } from 'url'
+
 const copy = async () => {
-    // Write your code here 
+    const __dirname =  path.dirname(fileURLToPath(import.meta.url));
+    const sourceDir = path.join(__dirname, 'files');
+    const targetDir = path.join(__dirname, 'files_copy');
+    const sourceFiles = await fs.readdir(sourceDir);
+    try {
+        await fs.access(targetDir);
+        throw new Error('FS operation failed');
+     } catch (error) {
+       if (error.code === "ENOENT") {
+        await fs.mkdir(targetDir, { recursive: true });
+        sourceFiles.forEach((file) => {
+            try {
+              fs.copyFile(path.join(sourceDir, file), path.join(targetDir, file));
+            } catch (err) {
+                throw error;
+            }
+          });
+       } else {
+         throw error;
+     }
+     }
 };
 
 await copy();
