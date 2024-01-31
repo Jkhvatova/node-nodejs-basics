b60cb2b053c2f96e3ea7ddaebca7ddc9f2423818
Julia Khvatova <jkhvatova@gmail.com>
2024-01-31 11:47:00 +0300

    feat: solve streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,6 +1,17 @@
-const write = async () => {
-    // Write your code here
+import * as fs from 'node:fs';
+import path from "node:path";
+import { fileURLToPath } from 'url'
 
+const write = async () => {
+    const __dirname =  path.dirname(fileURLToPath(import.meta.url));
+    const fileToWrite = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+    const stream = new fs.WriteStream(fileToWrite, 'utf-8');
+    console.log("Please write anything you want. If you want to stop writing, press Ctrl+Z")
+    process.stdin.pipe(stream);
 };
 
 await write();
+
+
+wj
