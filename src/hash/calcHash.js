e91d893b7a2298201046286b0b8b344fb37764b2
Julia Khvatova <jkhvatova@gmail.com>
2024-01-31 10:49:11 +0300

    feat: solve hash/calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import * as fs from 'node:fs';
+import path from "node:path";
+import { fileURLToPath } from 'url'
+import crypto from 'crypto'
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname =  path.dirname(fileURLToPath(import.meta.url));
+    const readStream = fs.createReadStream(path.join(__dirname, 'files', 'fileToCalculateHashFor.txt'));
+    const hash = crypto.createHash('sha256');
+    readStream.on('data', function(data) {
+        hash.update(data);
+      });
+      readStream.on('end', function() {
+        console.log(hash.digest('hex'));
+      });
 };
 
 await calculateHash();
--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -3,7 +3,6 @@ import { release, version } from 'os';
 import { createServer as createServerHttp } from 'http';
 import { fileURLToPath } from 'url'
 import './files/c.js';
-import { assert } from "console";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname =  path.dirname(__filename);
