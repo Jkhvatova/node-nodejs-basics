72e3eabf4a4b3535d98a94c7025e9e738e020791
Julia Khvatova <jkhvatova@gmail.com>
2024-02-01 19:32:53 +0300

    feat: solve zip/decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import * as fs from 'node:fs';
+import path from "node:path";
+import { fileURLToPath } from 'url'
+import zlib from 'node:zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const unzip = zlib.createGunzip();
+    const __dirname =  path.dirname(fileURLToPath(import.meta.url));
+    const decompressedFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const compressedFile = path.join(__dirname, 'files', 'archive.gz');
+
+    try {
+        const streamToDecompress = fs.createReadStream(compressedFile);
+        const resultStream = fs.createWriteStream(decompressedFile, 'utf-8');
+        streamToDecompress.pipe(unzip).pipe(resultStream);
+    } catch (error) {
+        throw error;
+    }
 };
 
 await decompress();
