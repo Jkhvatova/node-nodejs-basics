15c307ff6a1bbe64a9cc7d09552469f17d3aba74
Julia Khvatova <jkhvatova@gmail.com>
2024-02-02 10:42:33 +0300

    feat: solve wt task

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -5,7 +5,7 @@ import { workerData, parentPort } from 'node:worker_threads';
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    const result = nthFibonacci(workerData.n);
+    const result = nthFibonacci(workerData);
     parentPort.postMessage({ result });
 };
 
