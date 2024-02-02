d1ebc8a37b0b803851c656cc8afeb65746ecaf6f
Julia Khvatova <jkhvatova@gmail.com>
2024-02-02 10:30:09 +0300

    feat: solve cp/cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
+import path from "node:path";
+import { fileURLToPath } from 'url';
+import { spawn } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __dirname =  path.dirname(fileURLToPath(import.meta.url));
+    const scriptFile = path.join(__dirname, 'files', 'script.js');
+
+    const child = spawn('node', [scriptFile, ...args], {
+        stdio: ['pipe', 'pipe', 'inherit']
+      });
+
+      process.stdin.pipe(child.stdin);
+      child.stdout.pipe(process.stdout);
+      child.on('end', () => process.exit(exitCode));
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['someArgument1', 'someArgument2']);
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,10 @@
+import path from "node:path";
+import { fileURLToPath } from 'url';
+import os from 'os';
+import {Worker, isMainThread, parentPort, workerData } from 'node:worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+
 };
 
 await performCalculations();
